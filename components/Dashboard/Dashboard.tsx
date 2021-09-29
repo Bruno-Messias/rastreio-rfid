d4ffab1c3311b085094b1cd06dac6b20cf9c19fb
Leonardo <leonardo.appio@gmail.com>
2021-09-29 15:42:14 -0300

    correcao de css na searchbar

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -2,24 +2,21 @@ import Etapa from "../../models/Etapa";
 import Workflow from "../../models/Workflow";
 import EtapasList from "./EtapasList";
 import SearchBar from "./SearchBar";
-import {WorkflowList} from "./WorkflowList";
+import { WorkflowList } from "./WorkflowList";
 
-interface DashboardProps{
-    workflows : Workflow[]
-    etapas : Etapa[]
+interface DashboardProps {
+    workflows: Workflow[]
+    etapas: Etapa[]
 }
 
 
-export default function Dashboard(props : DashboardProps){
-    return(
-        <div className = "flex items-stretch justify-between">
-            <WorkflowList workflows = {props.workflows}/>
+export default function Dashboard(props: DashboardProps) {
+    return (
+        <div className="flex items-stretch justify-between bg-blue-50">
+            <WorkflowList workflows={props.workflows} />
             <div>
-                <SearchBar/>
-                <h2>
-                    Etapas
-                </h2>
-                <EtapasList etapas = {props.etapas}/>
+                <SearchBar />
+                <EtapasList etapas={props.etapas} />
             </div>
         </div>
     )
--- components/Dashboard/EtapasList.tsx
+++ components/Dashboard/EtapasList.tsx
@@ -1,17 +1,23 @@
 import Etapa from "../../models/Etapa";
 import EtapaCard from "./EtapaCard";
 
-interface EtapasListProps{
-    etapas : Etapa[]
+interface EtapasListProps {
+    etapas: Etapa[]
 }
 
-export default function EtapasList(props : EtapasListProps){
-    return(
-        <div className = "flex flex-wrap items-stretch">
-            {props.etapas.map(etapa => (
-                <EtapaCard nomeEtapa={etapa.nome} quantidadeDeItens = {etapa.quantidadeDeItens}/>
-            ))}
+export default function EtapasList(props: EtapasListProps) {
+    return (
+        <div className = "rounded-lg p-6 shadow-xl bg-white m-3">
+            <h1 className="text-3xl ml-5">
+                Etapas
+            </h1>
+            <div className="flex flex-wrap items-stretch ">
+                {props.etapas.map(etapa => (
+                    <EtapaCard nomeEtapa={etapa.nome} quantidadeDeItens={etapa.quantidadeDeItens} />
+                ))}
+            </div>
         </div>
+
     )
 
 }
