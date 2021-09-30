83bcfedd0c1bf4fb6a55f44df9c54b21e70fda48
Leonardo <leonardo.appio@gmail.com>
2021-09-30 13:55:39 -0300

    estado do card de etapa arrumado

--- components/Dashboard/EtapasList.tsx
+++ components/Dashboard/EtapasList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Etapa from "../../models/Etapa";
 import EtapaCard from "./EtapaCard";
 
@@ -6,6 +7,9 @@ interface EtapasListProps {
 }
 
 export default function EtapasList(props: EtapasListProps) {
+
+    const [etapaAtiva, setEtapaAtiva] = useState(props.etapas[0]);
+    
     return (
         <div className = "rounded-lg p-6 shadow-xl bg-white m-2 ">
             <h1 className="text-3xl ml-5">
@@ -13,7 +17,11 @@ export default function EtapasList(props: EtapasListProps) {
             </h1>
             <div className="flex flex-wrap justify-center ">
                 {props.etapas.map(etapa => (
-                    <EtapaCard nomeEtapa={etapa.nome} quantidadeDeItens={etapa.quantidadeDeItens} />
+                    <EtapaCard
+                        nomeEtapa={etapa.nome} 
+                        quantidadeDeItens={etapa.quantidadeDeItens} 
+                        ativo = {etapaAtiva == etapa ? "true" : "false"} 
+                        onClick = {() => setEtapaAtiva(etapa)}/>
                 ))}
             </div>
         </div>
--- components/Dashboard/WorkflowList.tsx
+++ components/Dashboard/WorkflowList.tsx
@@ -10,6 +10,7 @@ interface WorkflowListProps {
 export const WorkflowList = (props: WorkflowListProps) => {
 
     const [wfAtivo, setWfAtivo] = useState(props.workflows[0]);
+
     return (
         <div className="rounded-lg p-1 pl-0 shadow-xl bg-white mt-28 mb-2 ml-2">
             <div className="text-3xl ml-10 mt-10">
@@ -22,14 +23,8 @@ export const WorkflowList = (props: WorkflowListProps) => {
             </div>
             <div className="flex flex-col p-10 ml-0">
                 {props.workflows.map(workflow => (
-                    workflow == wfAtivo ? (
-                        <WorkflowCard nomeWorkflow={workflow.nome} ativo="true" onClick = {() => setWfAtivo(workflow)} />
-                    ) : (
-                        <WorkflowCard nomeWorkflow={workflow.nome} ativo="false" onClick = {() => setWfAtivo(workflow)} />
-                    )
-
+                    <WorkflowCard nomeWorkflow={workflow.nome} ativo={workflow == wfAtivo ? "true" : "false"} onClick={() => setWfAtivo(workflow)} />
                 ))}
-
             </div>
 
         </div>
